f943fac076820e715807f707a5777a4c7de16427
João <thesleepyjo2@gmail.com>
2019-12-06 20:36:57 +0000

    feito

--- SIBD/storedprocedure.sql
+++ SIBD/storedprocedure.sql
@@ -1,28 +1,32 @@
 delimiter $$
 
-create procedure raise_doctor_salary(in x integer)
+create procedure raise_doctor_salary(in x int)
 begin
 	update employee 
     set employee_salary = employee_salary + 0.1 * employee_salary
     where employee_VAT in ( 
 	    select pd.employee_VAT
 	    from permanent_doctor as pd 
-	    where  years>= 10 )
+	    where  years>= x )
 		and employee_VAT in (select doctor_VAT
                              from consultation 
                              where doctor_VAT in(select doctor_VAT
                                                  from consultation
-                                                 group by doctor_VAT having count(*)>100)) 
+                                                 where year(date_timestamp)=year(now())
+                                                 group by doctor_VAT having count(*)>2));
 		
 	update employee 
     set employee_salary = employee_salary + 0.05 * employee_salary
     where employee_VAT in ( 
 	    select pd.employee_VAT
 	    from permanent_doctor as pd 
-	    where  years>= 10 ) and employee_VAT not in (select doctor_VAT
+	    where  years>= x ) and employee_VAT not in (select doctor_VAT
                              from consultation 
                              where doctor_VAT in(select doctor_VAT
                                                  from consultation
-                                                 group by doctor_VAT having count(*)>100)) 
+                                                 where year(date_timestamp)=year(now())
+                                                 group by doctor_VAT having count(*)>100));
 		
 end$$
+
+delimiter;
